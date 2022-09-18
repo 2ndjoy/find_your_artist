e3b333a584ec07e576c34baf7883cb49a4db62b1
joy <juniorbat0003@gmail.com>
2022-09-18 10:20:49 +0600

    done

--- script.js
+++ script.js
@@ -5,11 +5,12 @@
 const handleSearch = () => {
   const keyword=document.getElementById('keyword').value;
   console.log(keyword);
+  const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => showArtists(data));
 
-
+git 
 };
 
 const showArtists = (data) => {
